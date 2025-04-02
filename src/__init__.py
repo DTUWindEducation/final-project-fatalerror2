f2ef5446026ce9f5af515c1e56f85ac930fd2f73
Warren <warrenwang2000@gmail.com>
2025-04-02 21:47:10 +0200

    add plot function

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def load_data(PATH):
-    df = pd.read_csv(PATH)
-    return df.to_dict(orient='list')
+    dict = pd.read_csv(PATH)
+    return dict.to_dict(orient='list')
+
+def plot_relativehumidity_2m(PATH):
+    dict = load_data(PATH)
+    col1 = "Time"
+    col2 = "relativehumidity_2m"
+    plt.figure()
+    plt.plot(dict[col1],dict[col2])
+    plt.xlabel(col1)
+    plt.ylabel(col2)
+    plt.show()
