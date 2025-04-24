e733fd4286b72139d82b88f87962d55da6766c84
Warren <warrenwang2000@gmail.com>
2025-04-24 11:24:04 +0200

    import functions from src folder

--- examples/main.py
+++ examples/main.py
@@ -1,4 +1,11 @@
-from svm import *
+import os
+import sys
+
+# Add the parent directory (project root) to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src import train_and_save_svm, plot_prediction_vs_actual
+
 
 # Train using 4 lag terms
 mae, mse, rmse = train_and_save_svm(site_index=1, num_lags=5)
