d5c215c18aa6c0c1e156afa7e840b0b82e01d3b8
Warren <warrenwang2000@gmail.com>
2025-04-29 11:54:08 +0200

    add code to determine winner

--- examples/main.py
+++ examples/main.py
@@ -7,7 +7,7 @@ import time
 # Add the parent directory (project root) to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src import create_lstm_model
+from src import create_lstm_model, determine_winner
 from src.WindPowerForecaster import WindPowerForecaster
 
 # Start timing
@@ -16,8 +16,8 @@ execution_start_time = time.time()
 # --- User Configurations ---
 site_index = 1
 variable_name = "Power"
-start_time = "2017-01-20"
-end_time = "2017-01-21"
+start_time = "2017-01-06"
+end_time = "2017-02-10"
 
 forecaster = WindPowerForecaster(site_index=site_index, start_time=start_time, end_time=end_time)
 
@@ -36,31 +36,41 @@ print("\n--- Starting Machine Learning Models... ---")
 
 # --- Run SVM ---
 print("\n--- Loading SVM Model ...---")
-mae, mse, rmse = forecaster.train_and_save_svm(num_lags=num_lags)
+svm_mae, svm_mse, svm_rmse = forecaster.train_and_save_svm(num_lags=num_lags)
 print("\n--- Starting Prediction ... ---")
 
 print("\n--- SVM Evaluation ---")
-forecaster.print_evaluation_metrics(mae, mse, rmse)
+forecaster.print_evaluation_metrics(svm_mae, svm_mse, svm_rmse)
 
 forecaster.plot_svm_result(num_lags=num_lags)
 
 # --- Run LSTM ---
 print("\n--- Initializing Neural Network LSTM Training... ---")
 
-_, y_test, mse, mae, rmse, times = forecaster.plot_lstm_result(
+_, y_test, lstm_mse, lstm_mae, lstm_rmse, times = forecaster.plot_lstm_result(
     model_funct=create_lstm_model,
     lookback_hours=lookback_hours)
 
 print("\n--- LSTM Evaluation ---")
-forecaster.print_evaluation_metrics(mae, mse, rmse)
+forecaster.print_evaluation_metrics(lstm_mae, lstm_mse, lstm_rmse)
 
 # --- Persistence Model Evaluation ---
 print("\n--- Generating Persistance Model ---")
-mae, mse, rmse = forecaster.plot_persistence_result(np.array(y_test), times)
+persistence_mae, persistence_mse, persistence_rmse = forecaster.plot_persistence_result(np.array(y_test), times)
 print("\n--- Persistance Evaluation ---")
-forecaster.print_evaluation_metrics(mae, mse, rmse)
+forecaster.print_evaluation_metrics(persistence_mae, persistence_mse, persistence_rmse)
 
 
 # Compute and print total time
 total_time = time.time() - execution_start_time
 print(f"\nTotal execution time: {int(total_time // 60)} min {int(total_time % 60)} sec")
+
+# --- Collect all results ---
+models_metrics = {
+    'SVM': {'MAE': svm_mae, 'MSE': svm_mse, 'RMSE': svm_rmse},
+    'LSTM': {'MAE': lstm_mae, 'MSE': lstm_mse, 'RMSE': lstm_rmse},
+    'Persistence': {'MAE': persistence_mae, 'MSE': persistence_mse, 'RMSE': persistence_rmse}
+}
+
+# --- Determine winner ---
+determine_winner(models_metrics)
